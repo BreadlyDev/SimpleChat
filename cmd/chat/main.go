ef2d2517df6457911610022bdb8f010eac99bc5c
breadlydev <breadydev@gmail.com>
2024-11-20 22:06:35 +0600

    added logger

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"simplechat/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting simple-chat",
+		slog.String("env", cfg.Env),
+	)
 
 	fmt.Println(cfg)
 
@@ -21,4 +36,28 @@ func main() {
 	// TODO: Initialize App
 
 	// TODO: ...
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
